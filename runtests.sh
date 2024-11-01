9da0c910582e6c5b3e170ba079233d6add65088f
James Sharp <sharpstuff@hotmail.com>
2024-11-01 09:13:56 +0000

    merged coverage report with run tests

--- runtests.sh
+++ runtests.sh
@@ -1,5 +1,22 @@
 #! /bin/bash
 
+UNITPATH=./tests/*.py
+
+echo "* Missing coverage *"
+
+grep --no-filename 'def\s.*\(.*\)\:' *.py | grep -v '__init__' | while read line;
+do
+    filter=`echo "${line}" | sed "s/def //g" | awk -F'\(' '{print $1}'`
+
+    present=`grep $filter $UNITPATH`
+
+    if [ "$present" = "" ]; then
+        echo "${filter}"
+    fi
+done
+
+echo ""
+
 echo "Testing Instruction Set"
 python3 tests/instruction_set_unit.py
 
@@ -8,3 +25,4 @@ python3 tests/assembly_parser_unit.py
 
 echo "Testing Assembler"
 python3 tests/assembler_unit.py
+
